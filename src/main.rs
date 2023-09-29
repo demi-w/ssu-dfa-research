39426ec5983a8e97987945a7ff4542c777311bf4
Demi Willison <65977881+hbgg@users.noreply.github.com>
2023-09-29 13:30:37 -0700

    Incremental UI additions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use std::thread::JoinHandle;
 use std::time::Duration;
 use std::path::PathBuf;
 
+use gloo_file::ObjectUrl;
 
 
 
@@ -20,6 +21,14 @@ use srs_to_dfa::{wbf_fix,execute};
 use std::sync::mpsc::{Receiver, Sender};
 use rfd::{self, FileHandle};
 
+#[cfg(target_arch = "wasm32")]
+pub use web_time::Instant;
+
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use std::time::Instant;
+
+
 use srs_to_dfa::test::*;
 
 
@@ -29,29 +38,23 @@ use web_sys;
 
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-
-    let oh_dear = DFA::jflap_load(&mut File::open("jffresults/collatz_attempt.jff").unwrap());
+    let oh_dear = DFA::jflap_load(&mut File::open("jffresults/lessbadcollatz.jff").unwrap());
 
     let goal = DFA::jflap_load(&mut File::open("example_goals/collatz_cycle.jff").unwrap());
 
-    let mut rule_str ="".to_owned();
+    let mut ruleset_str = "".to_owned();
+    File::open("srs/collatz").unwrap().read_to_string(&mut ruleset_str);
+    let ruleset = Ruleset::from_string(&ruleset_str);
 
-    File::open("srs/collatz").unwrap().read_to_string(&mut rule_str).unwrap();
+    let solver = MinkidSolver::new(ruleset, goal);
 
-    let ruleset = Ruleset::from_string( &rule_str);
-
-    let solver = MinkidSolver::new(ruleset,goal);
-
-//    for str in solver.solve_string(&oh_dear, &oh_dear.symbol_set.string_to_symbols(&vec!["flag","start","0|1","1|V","end"])) {
-//        println!("{}",oh_dear.symbol_set.symbols_to_string(&str));
-//    }
-    let result_dfa = solver.run_with_print(7);
-    result_dfa.jflap_save(&mut File::create("jffresults/collatz_attempt.jff").unwrap());
+    solver.run_with_print(5);
 
     match solver.is_superset(&oh_dear) {
-        Ok(_) => println!("yippiee"),
-        Err((rgr,start,end)) => println!("{} | {} | {}",rgr.to_string(&solver.get_ruleset().symbol_set),start,end)
+        Ok(_) => {println!("yippie")}
+        Err(e) => {println!("{}",e.0.to_string(&solver.rules.symbol_set))}
     }
+
     let options = eframe::NativeOptions::default();
     eframe::run_native(
         "SRS Box",
@@ -90,14 +93,18 @@ struct MyApp {
     handle : Option<JoinHandle<DFA>>,
     phase_content : Vec<Vec<Duration>>,
     phase_idx : usize,
+    last_phase_msg : Instant,
     srs_text : String,
+    sig_k : usize,
+    max_duration : f64,
     goal : DFA,
     goal_name : String,
     rules : Ruleset,
     path_channel: (
         Sender<(String,FileHandle,OpenItem)>,
         Receiver<(String,FileHandle,OpenItem)>,
-    )
+    ),
+    blob_link : String
 }
 
 impl Default for MyApp {
@@ -111,11 +118,15 @@ impl Default for MyApp {
             handle : None,
             phase_content : vec![vec![]; MinkidSolver::get_phases().len()],
             phase_idx : 0,
+            sig_k : 5,
             srs_text : temp_solver.rules.to_string(),
             goal : temp_solver.goal,
             goal_name : "onlyone1.dfa".to_string(),
             rules : temp_solver.rules,
             path_channel : std::sync::mpsc::channel(),
+            last_phase_msg : Instant::now(),
+            max_duration : 0.0,
+            blob_link : "".to_owned()
         }
     }
 }
@@ -131,6 +142,7 @@ impl eframe::App for MyApp {
             if let Some(h) = handle {
                 if h.is_finished() {
                     self.final_dfa = Some(h.join().unwrap());
+                    self.blob_link = generate_obj_link(self.final_dfa.as_ref().unwrap());
                 } else {
                     self.handle = Some(h);
                 }                
@@ -166,8 +178,11 @@ impl eframe::App for MyApp {
             match &self.phase_reciever {
                 Some(k_phase_recv) => match k_phase_recv.try_recv() {
                     Ok(message) => {
+                        self.max_duration = message.as_secs_f64().max(self.max_duration);
                         self.phase_content[self.phase_idx].push(message);
                         self.phase_idx = (self.phase_idx + 1) % MinkidSolver::get_phases().len();
+                        self.last_phase_msg = Instant::now();
+                        
                     }
                     Err(_) => {
                         break
@@ -191,6 +206,7 @@ impl eframe::App for MyApp {
                                 if cfg!(target_arch = "wasm32") {
                                     let event = self.dfa_content.as_ref().unwrap();
                                     self.final_dfa = Some(srs_to_dfa::solver::event_to_dfa(&event.0,&event.1, &self.rules));
+                                    self.blob_link = generate_obj_link(self.final_dfa.as_ref().unwrap());
                                 }
                                 self.dfa_reciever = None
                             },
@@ -209,17 +225,28 @@ impl eframe::App for MyApp {
 
 
         if let Some(_) = self.phase_reciever {
-            egui::Window::new("Construction Progress").show(ctx, |ui| {
+            let mut title = "Construction Progress - ".to_owned();
+            title.push_str(&MinkidSolver::get_phases()[self.phase_idx]);
+            egui::Window::new(title).show(ctx, |ui| {
                 Plot::new("my_plot")
-                    .set_margin_fraction(egui::Vec2 { x: 0.1, y: 0.1 })
                     .legend(Legend::default())
-                    .view_aspect(2.0).show(ui, |plot_ui| 
+                    .view_aspect(2.0)
+                    .auto_bounds_x()
+                    .auto_bounds_y()
+                    .include_x(0.0)
+                    .include_y(0.0)
+                    .include_x(self.phase_content[0].len() as f32)
+                    .include_y(self.max_duration)
+                    .show(ui, |plot_ui| 
                     {
                         
                         for (i, name) in MinkidSolver::get_phases().iter().enumerate() {
                             let mut points = vec![[0.0,0.0];self.phase_content[i].len()];
                             for j in 0..self.phase_content[i].len() {
                                 points[j] = [j as f64,self.phase_content[i][j].as_secs_f64()]
+                            }
+                            if self.dfa_reciever.is_some() && self.phase_idx == i {
+                                points.push([points.len() as f64,(Instant::now() - self.last_phase_msg).as_secs_f64()]);
                             }
                             plot_ui.line(Line::new(points).name(name));
                         }
@@ -249,12 +276,27 @@ impl eframe::App for MyApp {
                 let temp_solver = MinkidSolver::new(self.rules.clone(), self.goal.clone());
                 self.final_dfa = None;
                 self.phase_content = vec![vec![]; MinkidSolver::get_phases().len()];
-                self.run_dfa(temp_solver, 5);
+                self.last_phase_msg = Instant::now();
+                self.max_duration = 0.0;
+                self.run_dfa(temp_solver, self.sig_k);
+                Plot::new("my_plot").reset();
             }
+            let mut tmp_value = if self.sig_k == 0 {"".to_owned()} else {format!("{}", self.sig_k)};
+            ui.label("Signature Set Size: ");
+            let res = ui.text_edit_singleline(&mut tmp_value);
+            if tmp_value == "" {
+                self.sig_k = 0;
+            }else if let Ok(result) = tmp_value.parse() {
+                self.sig_k = result;
+            }
+            
             if let Some(dfa) = &self.final_dfa {
                 ui.label(format!("{} States",dfa.state_transitions.len()));
                 if ui.button("Save DFA").clicked() {
                     save_dfa(self.final_dfa.as_ref().unwrap().clone())
+                }
+                if cfg!(target_arch = "wasm32") {
+                    ui.hyperlink(&self.blob_link);
                 }
             }
 
@@ -329,12 +371,27 @@ fn save_dfa(dfa : DFA) {
     };
     execute(async_f);
 }
+/* 
+#[cfg(not(target_arch = "wasm32"))]
+fn generate_obj_link(dfa : &DFA) -> String {
+    "".to_owned()
+}*/
 
 
 
+#[cfg(target_arch = "wasm32")]
+fn generate_obj_link(dfa : &DFA) -> String {
+    let jeez = dfa.save_jflap_to_bytes();
+    let ew = String::from_utf8_lossy(&jeez);
+    let awk = ew.as_ref();
+    let blob = gloo_file::File::new_with_options("result.jff",awk,Some("text/plain"),None);
+    ObjectUrl::from(blob).to_string()
+}
 
-
-
+#[cfg(not(target_arch = "wasm32"))]
+fn generate_obj_link(dfa : &DFA) -> String {
+    "this shouldn't be visible.".to_owned()
+}
 
 
 /*#[cfg(not(target_arch = "wasm32"))]
