58122e1f935ef3240abc5bb190371a56d094ef34
Demi Willison <65977881+hbgg@users.noreply.github.com>
2023-09-29 15:33:37 -0700

    Split ruleset definition into its own file

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,7 +1,8 @@
 mod dfa;
 mod symset;
+mod ruleset;
 
 pub use crate::util::dfa::DFA;
 pub use crate::util::symset::SymbolSet;
 pub use crate::util::symset::SymbolIdx;
-pub use crate::util::symset::Ruleset;
+pub use crate::util::ruleset::Ruleset;
